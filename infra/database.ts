765044b8c12f18acc364842e3b8cb707425c37af
marcos lott <lott.marcos@gmail.com>
2025-03-26 21:08:42 -0300

    revert: replace Pool with Client for database connections and update query handling

--- infra/database.ts
+++ infra/database.ts
@@ -1,43 +1,39 @@
-import { Pool } from 'pg';
+import { Client } from 'pg';
 
-const IS_POOL_MODE = process.env.POSTGRES_POOL_MODE === 'transaction';
+async function getClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl:
+      process.env.POSTGRES_SSLMODE === 'require' ||
+      process.env.NODE_ENV === 'production',
+  });
 
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT),
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl:
-    process.env.POSTGRES_SSLMODE === 'require'
-      ? { rejectUnauthorized: false }
-      : false,
-  max: IS_POOL_MODE ? 20 : 1,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-  options: IS_POOL_MODE ? '-c pool_mode=transaction' : undefined,
-});
+  await client.connect();
+
+  return client;
+}
 
 async function query(
   queryObject:
     | string
     | { text: string; name?: string; values?: unknown[]; rowMode?: 'array' }
 ) {
-  const client = await pool.connect();
+  let client: Client;
 
   try {
+    client = await getClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error('[Source database.ts@query()]', error);
     throw error;
   } finally {
-    client.release();
+    await client.end();
   }
 }
 
-async function getClient() {
-  return await pool.connect();
-}
-
-export { query, getClient, pool };
+export { query, getClient };
